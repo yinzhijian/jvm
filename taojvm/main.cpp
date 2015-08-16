08b17dbd2a27a42012ed8811df0d075cd64e6030
yintao <373141588@qq.com>
2015-08-16 23:19:14 +0800

    Initial Commit

---
+++ taojvm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  taojvm
+//
+//  Created by yintao on 15/8/16.
+//  Copyright (c) 2015年 yintao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
